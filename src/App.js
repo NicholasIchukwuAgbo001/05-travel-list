7f0c67d6195774c169885de2a13feeb5c1ad098d
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-06-30 02:33:23 +0100

    COMMIT

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ function Logo(){
    return <h1>🌳 Far Away 💼</h1> 
 }
 
-function Form(){}
+function Form(){
+    return <div className="add-form">
+        <h3>What do you need for your trip? 😻</h3>
+    </div>
+}
 
-function PackingList(){}
+function PackingList(){
+    return <div className="list">
+        list
+    </div>
+}
 
 function Stats(){}
