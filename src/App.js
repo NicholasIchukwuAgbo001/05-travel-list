1ba02e413d7cec1913bf938ea63a97082683efa5
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-06-30 13:25:04 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "charger", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -17,9 +18,22 @@ function Logo(){
 }
 
 function Form(){
-    return <div className="add-form">
-        <h3>What do you need for your trip? 😻</h3>
-    </div>
+    
+    function handleSubmit(event){
+        event.preventDefault();
+    }
+    return (
+        <form className="add-form" onSubmit={handleSubmit}>
+            <h3>What do you need for your trip? 😻</h3>
+            <select>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+            <input type="text" placeholder="item.."/>
+            <button>Add</button>
+        </form>
+     )
 }
 
 function PackingList(){
@@ -37,9 +51,10 @@ function PackingList(){
 function Item ({ item }) {
     return (
         <li> 
-            <span>
+            <span style = {item.packed ? {textDecoration: 'line-through'} : {}}>
                 {item.quantity} {item.description}
             </span>
+            <button>❌</button>
         </li>
     )
 }
